5a58aec0ccd2452b17ae3d7dfa22298e81c75706
joshel167 <54351116+joshel167@users.noreply.github.com>
2020-01-26 16:50:50 +0100

    jag commitade

--- db.php
+++ db.php
@@ -1,2 +1,14 @@
 <?php
-echo "hej";
+function connect()
+{
+    try {
+        $dsn = "sqlite:" . __DIR__ . "/data.sqlite";
+        $db = new PDO($dsn);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db;
+    } catch (PDOException $e) {
+        echo $e->errorInfo;
+        exit();
+    }
+}
