0c27fb0a9ece554b959aff4685cca67e33ca4613
joshel167 <54351116+joshel167@users.noreply.github.com>
2020-01-29 10:24:25 +0100

    asd

--- controller/index.php
+++ controller/index.php
@@ -3,4 +3,5 @@ require "../db/db.php";
 if($_SESSION["loggedIn"] == false or null){
     $_SESSION["loggedIn"] = false;
 }
+$posts = fetchall("select * from posts");
 require "../views/index.view.php";
--- views/index.view.php
+++ views/index.view.php
@@ -11,68 +11,78 @@
 <body>
 
 <div class="wrapper">
-<!------------------------HEADER start------------------------->
+    <!------------------------HEADER start------------------------->
     <div class="header">
-     <h1>Zender</h1>
+        <h1>Zender</h1>
         <hr>
-     <h2>"Send your thoughts and knowledge"</h2>
+        <h2>"Send your thoughts and knowledge"</h2>
 
-     <div class="navBar">
-         <!-- Login button  -->
-         <?php if($_SESSION["loggedIn"] == false){ ?>
-             <button class="userBtn" > Login </button>
-         <?php } ?>
+        <div class="navBar">
+            <!-- Login button  -->
+            <?php if ($_SESSION["loggedIn"] == false) { ?>
+                <button class="userBtn"> Login</button>
+            <?php } ?>
 
-         <!-- Register button  -->
-         <?php if($_SESSION["loggedIn"] == false){ ?>
-         <button class="userBtn"> Register </button>
-         <?php } ?>
+            <!-- Register button  -->
+            <?php if ($_SESSION["loggedIn"] == false) { ?>
+                <button class="userBtn"> Register</button>
+            <?php } ?>
 
-         <!-- Logout button  -->
-         <?php if($_SESSION["loggedIn"] == true){ ?>
-             <button class="userBtn" ><a href="../controller/logout.php">Logout</a></button>
-         <?php } ?>
+            <!-- Logout button  -->
+            <?php if ($_SESSION["loggedIn"] == true) { ?>
+                <button class="userBtn"><a href="../controller/logout.php">Logout</a></button>
+            <?php } ?>
 
-         <!-- New Post button  -->
-         <?php if($_SESSION["loggedIn"] == true){ ?>
-             <button class="userBtn" ><a href="../controller/newPost.php">New Post</a> </button>
-         <?php } ?>
-     </div>
+            <!-- New Post button  -->
+            <?php if ($_SESSION["loggedIn"] == true) { ?>
+                <button class="userBtn"><a href="../controller/newPost.php">New Post</a></button>
+            <?php } ?>
+        </div>
     </div>
-<!------------------------HEADER end------------------------->
+    <!------------------------HEADER end------------------------->
 
     <div class="userForms">
 
         <?php
-        if ($_SESSION["loggedIn"] == false){?>
-     <div class="userForm">
-        <form id="loginbox" action="login.php" method="POST">
-            <h3>Log in</h3>
-            <label for="username">Username:</label>
-            <input id="usrnBox" type="text" id="username" name="username" required>
-            <label for="password">Password:</label>
-            <input id="pwBox" type="password" id="password" name="password" required>
-            <input id="submit" type="submit" value="Submit">
-        </form>
-     </div>
+        if ($_SESSION["loggedIn"] == false) {
+            ?>
+            <div class="userForm">
+                <form id="loginbox" action="login.php" method="POST">
+                    <h3>Log in</h3>
+                    <label for="username">Username:</label>
+                    <input id="usrnBox" type="text" id="username" name="username" required>
+                    <label for="password">Password:</label>
+                    <input id="pwBox" type="password" id="password" name="password" required>
+                    <input id="submit" type="submit" value="Submit">
+                </form>
+            </div>
         <?php } ?>
 
         <?php
-        if ($_SESSION["loggedIn"] == false){?>
-     <div class="userForm">
-        <form id="registerbox" action="../controller/storeUser.php" method="POST">
-            <h3>Register</h3>
-            <input type="hidden" id="id" name="id">
-            <label for="username">Username:</label>
-            <input id="usrnBox" type="text" id="username" name="username" required>
-            <label for="password">Password:</label>
-            <input id="pwBox" type="password" id="password" name="password" required>
-            <input id="submit" type="submit" value="Submit">
-        </form>
-     </div>
+        if ($_SESSION["loggedIn"] == false) {
+            ?>
+            <div class="userForm">
+                <form id="registerbox" action="../controller/storeUser.php" method="POST">
+                    <h3>Register</h3>
+                    <input type="hidden" id="id" name="id">
+                    <label for="username">Username:</label>
+                    <input id="usrnBox" type="text" id="username" name="username" required>
+                    <label for="password">Password:</label>
+                    <input id="pwBox" type="password" id="password" name="password" required>
+                    <input id="submit" type="submit" value="Submit">
+                </form>
+            </div>
         <?php } ?>
 
     </div>
+    <?php foreach ($posts as $post){?>
+    <p><?=$post["description"] ?></p>
+    <p><?=$post["content"] ?></p>
+    <p><?=$post["dates"] ?></p>
+    <p><?=$post["user"] ?></p>
+    <p></p>
+    <?php } ?>
+
 </div>
 </body>
 </html>
