4168fe555ced42c1f2703c675b209fea03b51c21
joshel167 <54351116+joshel167@users.noreply.github.com>
2020-01-29 10:44:23 +0100

    awdsw

--- views/newPost.view.php
+++ views/newPost.view.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>New Post</title>
 </head>
 <body>
@@ -13,8 +14,10 @@
     <label for="description">Header:</label>
     <input type="text" name="description" id="description" required>
     <label for="content">Content:</label>
-    <input type="text" name="content" id="content" required>
+    <textarea name="content" id="content"></textarea>
     <input type="submit" value="create post!" name="btnNewPost">
 </form>
+<script src="../tinymce/js/tinymce/tinymce.min.js"></script>
+<script src="../tiny.js"></script>
 </body>
 </html>
