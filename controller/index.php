8a879d0a83537c06c89636b3619f190e52a6887d
joshel167 <54351116+joshel167@users.noreply.github.com>
2020-02-12 10:19:06 +0100

    Push
    Push

--- controller/index.php
+++ controller/index.php
@@ -1,15 +1,17 @@
 <?php
 require "../Post.php";
-if($_SESSION["loggedIn"] == false or null){
+if ($_SESSION["loggedIn"] == false or null) {
     $_SESSION["loggedIn"] = false;
 }
 $posts2 = [];
-if(isset($_POST["btnSearch"])){
+if (isset($_POST["btnSearch"])) {
     $posts = new Post();
     $posts2 = $posts->search($_POST);
-}else if ($_GET["btnSortByDate"]){
+}
+if (isset($_GET["btnSortByDate"])) {
     $posts = new Post();
     $posts2 = $posts->fetchAll("select * from posts where 1 order by dates ");
+} else {
     $posts = new Database();
     $posts2 = $posts->fetchAll("select * from posts");
 
